482fbbd7d7b037ee37ad53d86ad3fb3c7392fd18
Hugobert van Borsten <scheissapple@Hugoberts-MacBook-Pro.local>
2015-09-11 23:59:28 +0200

    Initial Commit

---
+++ ex01_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ex01_1
+//
+//  Created by Hugobert van Borsten on 11.09.15.
+//  Copyright (c) 2015 Hugobert van Borsten. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
